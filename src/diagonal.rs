5592d65dc6371a5091d706d1e7e1a4adf05bbfb7
Andrea Cracco <guilucand@gmail.com>
2024-06-14 13:23:05 +0300

    Add erp distance

--- src/diagonal.rs
+++ src/diagonal.rs
@@ -1,20 +1,21 @@
 use crate::distances::compute_distance;
 
-
-
-
-fn diagonal_distance(a: &[f64], b: &[f64], init_val: f64, dist_lambda: impl Fn(f64, f64, f64, f64, f64) -> f64) -> f64 {
-
-    let (a, b) = if a.len() > b.len() {
-        (b, a)
-    } else {
-        (a, b)
-    };
+fn diagonal_distance(
+    a: &[f64],
+    b: &[f64],
+    init_val: f64,
+    dist_lambda: impl Fn(f64, f64, f64, f64, f64) -> f64,
+) -> f64 {
+    let (a, b) = if a.len() > b.len() { (b, a) } else { (a, b) };
     // let a = b;
 
     let square_len = a.len();
 
-    let mut matrix = [vec![init_val; square_len], vec![init_val; square_len], vec![init_val; square_len]];
+    let mut matrix = [
+        vec![init_val; square_len],
+        vec![init_val; square_len],
+        vec![init_val; square_len],
+    ];
     matrix[0][0] = 0.0;
 
     // println!("{:?}", matrix[0]);
@@ -38,13 +39,7 @@ fn diagonal_distance(a: &[f64], b: &[f64], init_val: f64, dist_lambda: impl Fn(f
             let y = matrix[(i - 2) % 3][j + y_offset - 1];
             let z = matrix[(i - 1) % 3][j + xz_offset];
 
-            matrix[i % 3][j] = dist_lambda(
-                a[a_start - j], 
-                b[b_start + j],
-                x,
-                y,
-                z
-            );
+            matrix[i % 3][j] = dist_lambda(a[a_start - j], b[b_start + j], x, y, z);
         }
 
         // println!("{:?}", matrix[i % 3])
@@ -55,15 +50,49 @@ fn diagonal_distance(a: &[f64], b: &[f64], init_val: f64, dist_lambda: impl Fn(f
 
 #[pyo3::pyfunction]
 #[pyo3(signature = (x1, x2=None, cached=false, n_jobs=-1))]
-pub fn dtw_diag(x1: Vec<Vec<f64>>, x2: Option<Vec<Vec<f64>>>, cached: bool, n_jobs: i32) -> pyo3::PyResult<Vec<Vec<f64>>> {
+pub fn dtw_diag(
+    x1: Vec<Vec<f64>>,
+    x2: Option<Vec<Vec<f64>>>,
+    cached: bool,
+    n_jobs: i32,
+) -> pyo3::PyResult<Vec<Vec<f64>>> {
+    let distance_matrix = compute_distance(
+        |a, b| {
+            diagonal_distance(a, b, f64::INFINITY, |a, b, x, y, z| {
+                let dist = (a - b).powi(2);
+                dist + z.min(x.min(y))
+            })
+        },
+        x1,
+        x2,
+        n_jobs,
+    );
 
-    let distance_matrix = compute_distance(|a, b| diagonal_distance(a, b, f64::INFINITY, |a, b, x, y, z| {
-        let dist = (a - b).powi(2);
-        dist + z.min(x.min(y))
-    }), x1, x2, n_jobs);
-    
     Ok(distance_matrix)
+}
 
+#[pyo3::pyfunction]
+#[pyo3(signature = (x1, x2=None, gap_penalty = 1.0, cached=false, n_jobs=-1))]
+pub fn erp_diag(
+    x1: Vec<Vec<f64>>,
+    x2: Option<Vec<Vec<f64>>>,
+    gap_penalty: f64,
+    cached: bool,
+    n_jobs: i32,
+) -> pyo3::PyResult<Vec<Vec<f64>>> {
+    let distance_matrix = compute_distance(
+        |a, b| {
+            diagonal_distance(a, b, f64::INFINITY, |a, b, x, y, z| {
+                (y + (a - b).abs())
+                    .min((x + (b - gap_penalty).abs()).min(z + (a - gap_penalty).abs()))
+            })
+        },
+        x1,
+        x2,
+        n_jobs,
+    );
+
+    Ok(distance_matrix)
 }
 
 #[test]
