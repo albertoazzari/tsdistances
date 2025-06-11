d0f50b5f58759c9b81e0bd6b07d9377d49bddcaf
Alberto Azzari <alberto.azzari@univr.it>
2025-06-11 09:31:12 +0200

    Update dependencies to use git source for tisdistance_gpu and modify tests to load data from files

--- tests/test_correctness_cpu.py
+++ tests/test_correctness_cpu.py
@@ -19,119 +19,88 @@ from aeon import distances as aeon
 import stumpy
 import time
 
-X = np.loadtxt('tests/ACSF1/ACSF1_TRAIN.tsv', delimiter='\t')
+A = np.loadtxt('tests/ACSF1/ACSF1_TRAIN.tsv', delimiter='\t')[:10]
+B = np.loadtxt('tests/ACSF1/ACSF1_TEST.tsv', delimiter='\t')[:15]
 band = 1.0
 
 
-def check_metric(D, X):
-    # Check that the distance matrix is symmetric, has zeros on the diagonal, and all elements are positive
-    assert np.allclose(D, D.T, atol=1e-8), "Distance matrix is not symmetric"
-    assert np.allclose(np.diag(D), np.zeros(X.shape[0]), atol=1e-8), "Diagonal elements are not zeros"
-    assert np.all(D >= 0), "Distance matrix has negative elements"
-
-
 def test_euclidean_distance():
-    D = euclidean_distance(X, None, n_jobs=-1)
-    check_metric(D, X)
-    aeon_D = aeon.euclidean_pairwise_distance(X)
+    D = euclidean_distance(A, B, n_jobs=-1)
+    aeon_D = aeon.euclidean_pairwise_distance(A, B)
     assert np.allclose(D, aeon_D, atol=1e-8)
-
-
-def test_catcheucl_distance():
-    D = catcheucl_distance(X, None, n_jobs=-1)
-    check_metric(D, X)
-    
 
 def test_erp_distance():
     gap_penalty = 0.0
-    D = erp_distance(X, None, gap_penalty=gap_penalty, band=band, n_jobs=-1)
-    check_metric(D, X)
-    aeon_D = aeon.erp_pairwise_distance(X, g=gap_penalty, window=band)
+    D = erp_distance(A, B, gap_penalty=gap_penalty, band=band, n_jobs=-1)
+    aeon_D = aeon.erp_pairwise_distance(A, B, g=gap_penalty, window=band)
     assert np.allclose(D, aeon_D, atol=1e-8)
 
 
 def test_lcss_distance():
     epsilon = 0.1
-    start_time = time.time()
-    D = lcss_distance(X, None, epsilon=epsilon, band=band, n_jobs=-1)
-    end_time = time.time()
-    print(f"LCSS distance computation time: {end_time - start_time:.2f} seconds")
-    exit(0)
-    check_metric(D, X)
-    aeon_D = aeon.lcss_pairwise_distance(X, epsilon=epsilon, window=band)
+    D = lcss_distance(A, B, epsilon=epsilon, band=band, n_jobs=-1)
+    aeon_D = aeon.lcss_pairwise_distance(A, B, epsilon=epsilon, window=band)
     assert np.allclose(D, aeon_D, atol=1e-8)
 
 
 def test_dtw_distance():
-    D = dtw_distance(X, None, band=band, n_jobs=-1)
-    check_metric(D, X)
-    aeon_D = aeon.dtw_pairwise_distance(X, window=band)
+    D = dtw_distance(A, B, band=band, n_jobs=-1)
+    aeon_D = aeon.dtw_pairwise_distance(A, B, window=band)
     assert np.allclose(D, aeon_D, atol=1e-8)
 
 
 def test_ddtw_distance():
-    D = ddtw_distance(X, None, band=band, n_jobs=-1)
-    check_metric(D, X)
-    aeon_D = aeon.ddtw_pairwise_distance(X, window=band)
+    D = ddtw_distance(A, B, band=band, n_jobs=-1)
+    aeon_D = aeon.ddtw_pairwise_distance(A, B, window=band)
     assert np.allclose(D, aeon_D, atol=1e-8)
 
 
 def test_wdtw_distance():
     g = 0.05
-    D = wdtw_distance(X, None, g=g, band=band, n_jobs=-1)
-    check_metric(D, X)
-    aeon_D = aeon.wdtw_pairwise_distance(X, g=g, window=band)
+    D = wdtw_distance(A, B, g=g, band=band, n_jobs=-1)
+    aeon_D = aeon.wdtw_pairwise_distance(A, B, g=g, window=band)
     assert np.allclose(D, aeon_D, atol=1e-8)
 
 
 def test_wddtw_distance():
     g = 0.05
-    D = wddtw_distance(X, None, g=g, band=band, n_jobs=-1)
-    check_metric(D, X)
-    aeon_D = aeon.wddtw_pairwise_distance(X, g=g, window=band)
+    D = wddtw_distance(A, B, g=g, band=band, n_jobs=-1)
+    aeon_D = aeon.wddtw_pairwise_distance(A, B, g=g, window=band)
     assert np.allclose(D, aeon_D, atol=1e-8)
 
 
 def test_adtw_distance():
     warp_penalty = 1.0
-    D = adtw_distance(X, None, band=band, warp_penalty=warp_penalty, n_jobs=-1)
-    check_metric(D, X)
-    aeon_D = aeon.adtw_pairwise_distance(X, window=band, warp_penalty=warp_penalty)
+    D = adtw_distance(A, B, band=band, warp_penalty=warp_penalty, n_jobs=-1)
+    aeon_D = aeon.adtw_pairwise_distance(A, B, window=band, warp_penalty=warp_penalty)
     assert np.allclose(D, aeon_D, atol=1e-8)
 
 
 def test_msm_distance():
-    D = msm_distance(X, None, band=band, n_jobs=-1)
-    check_metric(D, X)
-    aeon_D = aeon.msm_pairwise_distance(X, window=band)
+    D = msm_distance(A, B, band=band, n_jobs=-1)
+    aeon_D = aeon.msm_pairwise_distance(A, B, window=band)
     assert np.allclose(D, aeon_D, atol=1e-8)
 
 
 def test_twe_distance():
     stiffness = 0.1
     penalty = 0.1
-    D = twe_distance(X, None, band=band, stifness=stiffness, penalty=penalty, n_jobs=-1)
-    check_metric(D, X)
-    aeon_D = aeon.twe_pairwise_distance(X, nu=stiffness, lmbda=penalty, window=band)
+    D = twe_distance(A, B, band=band, stifness=stiffness, penalty=penalty, n_jobs=-1)
+    aeon_D = aeon.twe_pairwise_distance(A, B, nu=stiffness, lmbda=penalty, window=band)
     assert np.allclose(D, aeon_D, atol=1e-8)
 
 
 def test_sb_distance():
-    D = sb_distance(X, None, n_jobs=-1)
-    check_metric(D, X)
-    aeon_D = aeon.sbd_pairwise_distance(X)
+    D = sb_distance(A, B, n_jobs=-1)
+    aeon_D = aeon.sbd_pairwise_distance(A, B)
     assert np.allclose(D, aeon_D, atol=1e-8)
 
 
 def test_mp_distance():
-    window = int(0.1 * X.shape[1])
-    D = mp_distance(X, window, None, n_jobs=-1)
-    check_metric(D, X)
+    window = int(0.1 * A.shape[1])
+    D = mp_distance(A, window, B, n_jobs=-1)
     D_stumpy = np.zeros_like(D)
-    for i in range(X.shape[0]-1):
-        for j in range(i+1, X.shape[0]):
-            D_stumpy[i, j] = stumpy.mpdist(X[i], X[j], m=window)
-            D_stumpy[j, i] = D_stumpy[i, j]
-    # Set D_stumpy diagonal to zero
-    np.fill_diagonal(D_stumpy, 0)
+    for i in range(A.shape[0]):
+        for j in range(B.shape[0]):
+            D_stumpy[i, j] = stumpy.mpdist(A[i], B[j], m=window)
     assert np.allclose(D, D_stumpy, atol=1e-8)
